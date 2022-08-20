cc408b38f1cfdf24ee77ad136cdccebd7d605fc8
Youssef-Projects <111628141+Youssef-Projects@users.noreply.github.com>
2022-08-20 18:21:58 +0200

    Add files via upload

---
+++ Script.sh
@@ -0,0 +1,805 @@
+xdotool mousemove 100 100 click 1
+sleep 1
+xdotool key ctrl+alt+t
+sleep 1
+xdotool windowsize $(xdotool getactivewindow) 100% 100%
+sleep 1
+xdotool key ctrl+shift+o
+sleep 1
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'Hi Sahar, I am your personal assistant to tell you about Joes story \n
+Warning:-\nthis story is fictional, and any connection between it and reality is an illusion' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+    xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p1'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p1'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+
+#xdotool key Return
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/d'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+sleep 75
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno '1-Does he help her by physically hacking the university system and preparing the exam paper for her, or does he tell her I can not help, I am m sorry?' --yes-button 'Help' --no-button 'Not Help' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p2'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p2'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q1'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q2' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p3'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p3'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q2'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+#-----------------------------------------------------------------------------------------
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q3' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p4'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p4'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q3'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q4' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p5'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p5'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q4'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q5' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p6'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p6'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q5'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q6' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p7'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p7'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q6'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q7' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p8'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p8'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q7'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q8' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p9'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p9'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q8'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q9' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/p10'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/p10'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q9'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+sleep 100
+xdotool type 'clear'
+xdotool key Return
+#T2
+xdotool type "                                                                                          
+if (whiptail --title 'Example Dialog' --yesno 'q10' --yes-button 'I Agree' --no-button 'reject' 10 87); then
+
+
+ xdotool key ctrl+Tab
+    
+    xdotool key ctrl+c
+
+    xdotool key ctrl+Tab
+
+# fun 1
+xdotool type 'clear'
+xdotool key Return
+xdotool type 'cat /home/u/project/e'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+xdotool type 'festival --tts /home/u/project/e'
+
+xdotool key Return
+
+xdotool key ctrl+Tab
+
+
+else
+    echo 'User selected No, exit status was $?.'
+fi
+"
+# after Q
+xdotool key ctrl+Tab
+sleep 1
+xdotool type 'festival --tts /home/u/project/q10'
+
+xdotool key Return
+sleep 1
+xdotool key ctrl+Tab
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
